9f228cab967b35e14e5e7cb89424711d7711b865
hamzzi0097 <81970551+hamzzi0097@users.noreply.github.com>
2023-03-22 23:17:56 +0900

    Delete legacy code

--- main.js
+++ main.js
@@ -32,6 +32,9 @@ app.on('ready', function(){
 
   mainWindow.loadURL('file://' + __dirname + '/src/index.html');
 
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools() 
+
   mainWindow.on('closed', function(){
     // making the mainWindow null
 
@@ -45,59 +48,11 @@ app.on('ready', function(){
 
     dialog.showOpenDialog({
       properties:['openFile']
-    }),function(files){
+    }, function(files){
       if(files){
-        event.sender.send('selected-file',file[0]);
+        console.log('send!');
+        event.sender.send('selected-file',files[0]);
       }
-    }
+    })
   })
 })
-
-// function createWindow () {
-//   // Create the browser window.
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//         nodeIntegration: true,
-//         contextIsolation: false,
-//     },
-//   })
- 
-//   require('@electron/remote/main').initialize()
-//   require("@electron/remote/main").enable(win.webContents)
-
-//   // Load the index.html of the app.
-//   win.loadFile('src/index.html')
- 
-//   // Open the DevTools.
-//   win.webContents.openDevTools()
-// }
- 
-// // This method will be called when Electron has finished
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// // This method is equivalent to 'app.on('ready', function())'
-// app.whenReady().then(createWindow)
- 
-// // Quit when all windows are closed.
-// app.on('window-all-closed', () => {
-//   // On macOS it is common for applications and their menu bar
-//   // To stay active until the user quits explicitly with Cmd + Q
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
- 
-// app.on('activate', () => {
-//   // On macOS it's common to re-create a window in the 
-//   // app when the dock icon is clicked and there are no
-//   // other windows open.
-//   if (BrowserWindow.getAllWindows().length === 0) {
-//     createWindow()
-//   }
-// })
- 
-// // In this file, you can include the rest of your
-// // app's specific main process code. You can also
-// // put them in separate files and require them here.
--- src/index.js
+++ src/index.js
@@ -12,21 +12,3 @@ buttonCreated.addEventListener('click', function(event){
 ipc.on('selected-file', function(event, path){
 	console.log('Full Path:' + path);
 })
-
-// const remote = require('@electron/remote');
-
-// const dialog = remote.getGlobal('dialog');
-
-// document.getElementById('upload').onclick = function() {
-// 	console.log(dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }));
-// };
-
-// if (global.filepath && !file.canceled) {
-// fs.readFile(global.filepath, {encoding: 'utf-8'}, function(err,data) {
-// 	if (!err) {
-// 		console.log('received data: ' + data);
-// 	} else {
-// 		console.log(err);
-// 	}
-// });
-// }
